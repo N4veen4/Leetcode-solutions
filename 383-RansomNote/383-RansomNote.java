1c1937b4df6978c375fb558b132e2a18a57afc40
N4veen_x <naveenbalaji425@gmail.com>
2026-07-14 14:50:45 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,19 @@
+// Last updated: 14/07/2026, 14:50:44
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+        int[] arr1=new int[26];
+        
+
+        for(char c:ransomNote.toCharArray()){
+            arr1[c-'a']++;
+        }
+        for(char c:magazine.toCharArray()){
+            arr1[c-'a']--;
+        }
+        for(int n:arr1){
+            if(n >=1) return false;
+        }
+        return true;
+    }
+}
