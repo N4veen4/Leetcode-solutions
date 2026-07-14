6f4e56872c9bb01f91f62bb122a42f886a9f4cbd
N4veen_x <naveenbalaji425@gmail.com>
2026-07-14 14:49:27 +0530

    Create 1920-DetermineColorOfAChessboardSquare

---
+++ 1920-DetermineColorOfAChessboardSquare/1920-DetermineColorOfAChessboardSquare.java
@@ -0,0 +1,11 @@
+// Last updated: 14/07/2026, 14:49:27
+class Solution {
+    public boolean squareIsWhite(String c) {
+        return((c.charAt(0)+c.charAt(1))%2 != 0);//{
+    //         return true;
+    //     }
+    //     else return false;
+        
+    // }
+}
+}
