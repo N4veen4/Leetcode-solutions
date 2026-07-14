5b35c4686bfef06d9bb0e8927c279a14465f14eb
N4veen_x <naveenbalaji425@gmail.com>
2026-07-14 14:49:07 +0530

    Create 2502-SortThePeople

---
+++ 2502-SortThePeople/2502-SortThePeople.java
@@ -0,0 +1,21 @@
+// Last updated: 14/07/2026, 14:49:07
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        
+        for(int i=0;i<heights.length;i++){
+
+            for(int j=i+1;j<heights.length;j++){
+                if(heights[i] < heights[j]){
+                    int temp = heights[i];
+                    heights[i] = heights[j];
+                    heights[j] = temp;
+
+                    String temp1 = names[i];
+                     names[i] = names[j];
+                    names[j] = temp1;
+                }
+            }
+        }
+        return names;
+    }
+}
